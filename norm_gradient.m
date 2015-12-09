7b0cbe8784ccbc54f8af1362ced49dbabc5007cf
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-12-09 10:49:38 +0100

    Rangement

--- norm_gradient.m
+++
@@ -1,5 +0,0 @@
-function gradient=norm_gradient(A)
-[h,w]=size(A);
-%c'est bôoooooooooooooooooooooo <3
-gradient=4*A-(A(:,[2:w w])+A(:,[1 1:w-1])+A([2:h h],:)+A([1 1:h-1],:));
-
--- racine.m
+++
@@ -1,8 +0,0 @@
-function R=racine(A,eps,dx)
-[h,w]=size(A);
-Eps=eps*eps*ones(h,w);
-Rprov=1/(2*dx)*(A([2:h h],:)-A([1 1:h-1],:));
-Rprov2=Rprov.*Rprov;
-Rprav=1/(2*dx)*(A(:,[2:w w])-A(:,[1 1:w-1]));
-Rprav2=Rprav.*Rprav;
-R=Eps+Rprav2+Rprov2;
